cf95d41b79085ca12a30d2891c5a06b1883e8e48
Leah <leah@uxwebtech.com>
2021-10-28 12:36:41 +0300

    HW Class8 have issues adding openqa.selenium

---
+++ src/main/java/Solutions.java
@@ -0,0 +1,10 @@
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Solutions {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driever",  "C:\\Users\\Yelena\\Downloads\\chromedriver_win32\\chromedriver.exe");
+//        driver = new ChromeDriver();
+    }
+}
